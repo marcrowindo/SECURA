cce4d1f4e39dd891bfa8d47bfbbb6d0d532d3ce1
Simon Abraham <nomis223@gmail.com>
2018-03-01 17:11:40 +0100

    changed some stuff on the scroller, just needs some design-love

--- app/javascript/packs/slide.js
+++ app/javascript/packs/slide.js
@@ -2,16 +2,32 @@
 import Slider from 'bootstrap-slider'
 
   new Slider('#ex1', {
-    formatter: function(value) {
-      return 'Current value: ' + value;
-    }
+    tooltip: 'hide'
   });
 
-var sliderValue = document.querySelector('.slider-handle').getAttribute('aria-valuenow')
-console.log(sliderValue)
-sliderValue.addEventListener('change', function(value) {
-  console.log(value)
-})
+let MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+
+let slider = document.getElementById('ex1')
+let sliderValue = document.getElementById('slider-value')
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.type == "attributes") {
+      sliderValue.innerText = slider.getAttribute('data-value')
+    }
+  });
+});
+
+
+observer.observe(slider, {
+  attributes: true //configure it to listen to attribute changes
+});
+
+// sliderValue.addEventListener('change', function(e) {
+//   console.log(e)
+// })
+
+
 
 // JQuery
 // $('#ex1').slider({
@@ -19,3 +35,4 @@ sliderValue.addEventListener('change', function(value) {
 //     return 'Current value: ' + value;
 //   }
 // });
+
